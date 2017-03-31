00cef33f121fc8341efe3583050d5bb29772fdc6
ParadoxZero <sidhin.thomas@gmail.com>
2017-03-31 19:18:23 +0530

    added gitignore

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wpdb');
+define('DB_NAME', 'trymake');
 
 /** MySQL database username */
-define('DB_USER', 'wpuser');
+define('DB_USER', 'root');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
